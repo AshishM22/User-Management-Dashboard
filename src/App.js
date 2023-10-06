92ad17108d6803b1efad6c20954883ae6c4ec8b7
Ashish Munot <ashish.munot22@gmail.com>
2023-10-07 02:32:13 +0530

    Fixed  some Responsive issue

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import AdminPanel from "./components/AdminPanel/AdminPanel";
 
 function App() {
+  // Render the AdminPanel component as the main content of the app
   return <AdminPanel />;
 }
 
